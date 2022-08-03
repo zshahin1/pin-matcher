9e14cdefd17021faa6aa35ea90d1a17f76a141d4
zshahin1 <101985465+zshahin1@users.noreply.github.com>
2022-08-03 10:50:07 +0600

    solved

--- pin.js
+++ pin.js
@@ -39,13 +39,74 @@ function verifyPin() {
     const typedNumbers = document.getElementById('typed-numbers').value;
     const successMessage = document.getElementById('notify-success');
     const failError = document.getElementById('notify-fail');
+    const submit = document.getElementById('submit');
+    const submit2 = document.getElementById('submit-2');
+    const calcInput = document.getElementById('typed-numbers');
+
     if (pin == typedNumbers) {
         successMessage.style.display = 'block';
         failError.style.display = 'none';
+        calcInput.value = '';
+        
+
     }
     else {
         successMessage.style.display = 'none';
         failError.style.display = 'block';
+        submit.style.display = 'none';
+        submit2.style.display = 'block';
+        calcInput.value = '';
     }
 }
+function verifyPin2() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    const submit = document.getElementById('submit');
+    const submit2 = document.getElementById('submit-2');
+    const submit3 = document.getElementById('submit-3');
+    const calcInput = document.getElementById('typed-numbers');
+    if (pin == typedNumbers) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+        calcInput.value = '';
+
+    }
+    else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+        submit.style.display = 'none';
+        submit2.style.display = 'none';
+        submit3.style.display = 'block';
+        calcInput.value = '';
+    }
+}
+function verifyPin3() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    const submit = document.getElementById('submit');
+    const submit2 = document.getElementById('submit-2');
+    const submit3 = document.getElementById('submit-3');
+    const submit4 = document.getElementById('submit-4');
+    const calcInput = document.getElementById('typed-numbers');
+    if (pin == typedNumbers) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+        calcInput.value = '';
+        
+
+    }
+    else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+        submit2.style.display = 'none';
+        submit3.style.display = 'none';
+        submit4.style.display = 'block';
+        calcInput.value = '';
+    }
+}
+
 
